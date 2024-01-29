79ace0a259b5da652dc99b0e8e59656c2e89308f
Mahbub Ahmed <mahbub.ahmed@gmail.com>
2024-01-29 00:59:22 -0500

    Code Clean

--- LTKLite/LTKLiteApp.swift
+++ LTKLite/LTKLiteApp.swift
@@ -11,7 +11,8 @@ import SwiftUI
 struct LTKLiteApp: App {
     var body: some Scene {
         WindowGroup {
-            ContentView()
+            FeedView()
+                .padding()
         }
     }
 }

b609a17df7e240cea27a3bdc94e9532b97d05fac
Mahbub Ahmed <mahbub.ahmed@gmail.com>
2024-01-29 00:38:31 -0500

    Added ViewModel Protocol and ViewState

---
+++ LTKLite/Utility/ViewModel.swift
@@ -0,0 +1,12 @@
+//
+//  ViewModel.swift
+//  LTKLite
+//
+//  Created by Mahbub Ahmed on 1/28/24.
+//
+
+import Foundation
+
+public protocol ViewModel {
+    var network: NetworkService { get }
+}
---
+++ LTKLite/Utility/ViewState.swift
@@ -0,0 +1,14 @@
+//
+//  ViewState.swift
+//  LTKLite
+//
+//  Created by Mahbub Ahmed on 1/28/24.
+//
+
+import Foundation
+
+enum ViewState {
+    case loading
+    case loaded
+    case failure
+}

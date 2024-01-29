025dd50327ee98167df8fd338137abd060a300d5
Mahbub Ahmed <mahbub.ahmed@gmail.com>
2024-01-29 00:12:39 -0500

    Initial Setup

---
+++ LTKLite/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  LTKLite
+//
+//  Created by Mahbub Ahmed on 1/29/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ LTKLite/LTKLiteApp.swift
@@ -0,0 +1,17 @@
+//
+//  LTKLiteApp.swift
+//  LTKLite
+//
+//  Created by Mahbub Ahmed on 1/29/24.
+//
+
+import SwiftUI
+
+@main
+struct LTKLiteApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
